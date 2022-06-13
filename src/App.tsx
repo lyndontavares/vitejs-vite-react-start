409670f3c2b475080b21d5fabed659220a81eed3
lyndontavares <integraldominio@gmail.com>
2022-06-13 14:05:19 -0300

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import viteLogo from '/vite.svg';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -21,15 +21,13 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <p>Lyndon Tavares - 2022</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
